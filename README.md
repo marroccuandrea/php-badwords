7b1e2b47e4d435403fd674b73937e3cb72fb468f
marroccuandrea <marroccuandrea@yahoo.it>
2024-04-12 15:46:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+PHP badwords
+===
+
+### Descrizione
+Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.<br>
+Gestire il tutto con due file diversi.<br>
+Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.<br>
+Il secondo file riceverà la richiesta ed eseguirà queste operazioni:<br>
+- stampare a schermo il paragrafo e la sua lunghezza
+- stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Ricevo GET</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
